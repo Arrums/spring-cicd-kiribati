0cb82ddec3da26bacfa00517d7a657cf61be2b5f
Sufyan Shaharin <arrum.soerjo@gmail.com>
2022-05-18 10:56:12 +1000

    Spring Application

---
+++ src/main/java/com/example/springcicd/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.springcicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/greetings")
+public class GreetingController {
+
+	@GetMapping
+	public String greeting() {
+		return "Hello, there";
+	}
+	
+	@GetMapping(value ="/{name}") 
+	public String customGreeting(@PathVariable String name) {
+		return String.format("Hello, %s", name);  
+	}
+}
