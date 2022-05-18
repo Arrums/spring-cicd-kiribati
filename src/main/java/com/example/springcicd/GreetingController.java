24aa54cb7428acf39781c97c4bcf6548050deee5
Sufyan Shaharin <arrum.soerjo@gmail.com>
2022-05-18 11:05:04 +1000

    Refactor custom greeting

--- src/main/java/com/example/springcicd/GreetingController.java
+++ src/main/java/com/example/springcicd/GreetingController.java
@@ -16,6 +16,6 @@ public class GreetingController {
 	
 	@GetMapping(value ="/{name}") 
 	public String customGreeting(@PathVariable String name) {
-		return String.format("Hello, %s", name);  
+		return "Hello, " + name; 
 	}
 }
